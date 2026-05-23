d7f87a14e2cacae75a6c37d8375cab129129f6d2
jndev <284169289+jndeva@users.noreply.github.com>
2026-05-23 13:10:34 -0300

    componentes

---
+++ part1/src/App.jsx
@@ -0,0 +1,14 @@
+const App = () => {
+  const a = 10
+  const b = 20
+  console.log(a + b)
+  return (
+    <>
+      <p>hello world</p>
+      <p>{a} plus {b} is {a + b}</p>
+    </>
+  )
+
+}
+
+export default App
