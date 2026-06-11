959ad0ea237ccfd6ba95675489239c26f0b47eba
jndev <284169289+jndeva@users.noreply.github.com>
2026-06-11 14:15:37 -0300

    unicafe. agregando total de feedbacks, porcentajes y usando renderizado condicional

--- part1/src/unicafe/App.jsx
+++ part1/src/unicafe/App.jsx
@@ -8,28 +8,47 @@ const Button = ({ handleClick, text }) => {
     )
 }
 
+const StatisticLine = ({ text, value }) => {
+    return (
+        <div>
+            {text === 'positive' ? <p>{text} {value} %</p> : <p>{text} {value}</p>}
+        </div>
+    )
+}
+
+const Statistics = ({ good, neutral, bad, all, average, positive }) => {
+    return (
+        all === 0 ? <p>no feedback given</p> :
+            <div>
+                <h1>statistics</h1>
+                <StatisticLine text='good' value={good} />
+                <StatisticLine text='neutral' value={neutral} />
+                <StatisticLine text='bad' value={bad} />
+                <StatisticLine text='all' value={all} />
+                <StatisticLine text='average' value={average} />
+                <StatisticLine text='positive' value={positive} />
+            </div>
+    )
+}
+
 const App = () => {
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
-    const [all, setAll] = useState(0)
+    const all = good + neutral + bad
+    const average = all === 0 ? 0 : (good * 1 + neutral * 0 + bad * -1) / all
+    const positive = all === 0 ? 0 : (good / all) * 100
 
     const handleGood = () => {
-        const updatedGood = good + 1
-        setGood(updatedGood)
-        setAll(updatedGood + neutral + bad)
+        setGood(good + 1)
     }
 
     const handleNeutral = () => {
-        const updatedNeutral = neutral + 1
-        setNeutral(updatedNeutral)
-        setAll(updatedNeutral + good + bad)
+        setNeutral(neutral + 1)
     }
 
     const handleBad = () => {
-        const updatedBad = bad + 1
-        setBad(updatedBad)
-        setAll(updatedBad + good + neutral)
+        setBad(bad + 1)
     }
 
     return (
@@ -38,11 +57,14 @@ const App = () => {
             <Button handleClick={handleGood} text='good' />
             <Button handleClick={handleNeutral} text='neutral' />
             <Button handleClick={handleBad} text='bad' />
-            <h1>Statistics</h1>
-            <p>good {good}</p>
-            <p>neutral {neutral}</p>
-            <p>bad {bad}</p>
-            <p>all {all}</p>
+            <Statistics
+                good={good}
+                neutral={neutral}
+                bad={bad}
+                all={all}
+                average={average}
+                positive={positive}
+            />
         </div>
     )
 }
