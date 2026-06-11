0c38f3df0d16ab6cb961b5379f9c2df7f52e95b0
jndev <284169289+jndeva@users.noreply.github.com>
2026-06-11 18:20:14 -0300

    matrices y métodos de matrices

---
+++ part2/src/App.jsx
@@ -0,0 +1,16 @@
+function App({ notes }) {
+  return (
+    <>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note =>
+          <li key={note.id}>
+            {note.content}
+          </li>
+        )}
+      </ul>
+    </>
+  )
+}
+
+export default App
